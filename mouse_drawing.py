a81e89371661c4731f508e698e524cd6700e8108
Rodrigue Roland CODO <codorol22@gmail.com>
2022-09-02 19:40:50 +0500

    Drawing rectangle,circle,and curve with opencv mouse event

---
+++ mouse_drawing.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2 as cv
+import math
+drawing = False # true if mouse is pressed
+drawRectangle = True # if True, draw rectangle. Press 'm' to toggle to curve
+drawCircle = True # if True, draw circle
+ix,iy = -1,-1
+
+img = np.zeros((512,512,3), np.uint8)
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing,drawRectangle,drawCircle
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing:
+            if drawRectangle:
+                cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            elif drawCircle:
+                cv.circle(img,(ix,iy),int(math.sqrt(pow(x-ix,2)+pow(y-iy,2))),(255,0,0),-1)
+            else:
+                cv.circle(img,(x,y),5,(0,0,255),-1)
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        if drawRectangle:
+            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+        elif drawCircle:
+            cv.circle(img,(ix,iy),int(math.sqrt(pow(x-ix,2)+pow(y-iy,2))),(255,0,0),-1)
+        else:
+            cv.circle(img,(x,y),5,(0,0,255),-1)
+
+cv.namedWindow('image')
+cv.setMouseCallback('image',draw_circle)
+while(1):
+    cv.imshow('image',img)
+    k = cv.waitKey(1)
+    if k == ord('m'):
+        drawRectangle = not drawRectangle
+    elif k == ord('c'):
+        drawCircle = not drawCircle
+    elif k == ord('q'):
+        break
+cv.destroyAllWindows()
